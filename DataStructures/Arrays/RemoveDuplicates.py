9aa8598929eef077db300b7e476efc626e5680b9
Nicholas Koech <nicholask320@gmail.com>
2021-11-07 10:25:13 -0800

    Update RemoveDuplicates.py

--- DataStructures/Arrays/RemoveDuplicates.py
+++ DataStructures/Arrays/RemoveDuplicates.py
@@ -1,1 +1,13 @@
+class Solution:
+    def removeDuplicates(self, nums) -> int:
+         index=1
+         if len(nums)==2 and nums[0]!=nums[1]: 
+            return len(nums)
 
+         else:
+            index=1
+            for i in range(1,len(nums)):
+                if nums[i]!=nums[i-1]:
+                    nums[index]=nums[i]
+                    index+=1
+            return index
