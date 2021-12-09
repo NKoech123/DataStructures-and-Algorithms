13f48af1bdc5a8e1ed811edb4dff014c50911f19
Nicholas koech <nicholaskoech@Nicholass-MacBook-Pro.local>
2021-12-09 14:11:06 -0800

    First commit

--- DataStructures/Linkedlist/Single_Linkedlist_design.py
+++ DataStructures/Linkedlist/Single_Linkedlist_design.py
@@ -7,31 +7,52 @@ class ListNode:
 class MyLinkedList(object):
 
     def __init__(self):
+        """
+        Initialize your data structure here
+        """
         self.head = None
         self.size = 0
 
     def get(self, index):
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        :type index: int
+        :rtype: int
+        """
         if index < 0 or index >= self.size:
             return -1
         
         current = self.head
         
-        for _ in range(index):            
+        for i in range(0, index):            
             current = current.next
 
         return current.val
     
     def addAtHead(self, val):
-   
+        """
+        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+        :type val: int
+        :rtype: None
+        """
         self.addAtIndex(0, val)
 
     def addAtTail(self, val):
-        
+        """
+        Append a node of value val to the last element of the linked list.
+        :type val: int
+        :rtype: None
+        """
         self.addAtIndex(self.size, val)
         
 
     def addAtIndex(self, index, val):
-        
+        """
+        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+        :type index: int
+        :type val: int
+        :rtype: None
+        """
         if index > self.size:
             return
         
@@ -42,7 +63,7 @@ class MyLinkedList(object):
             newNode.next = current
             self.head = newNode
         else:                        
-            for _ in range(index - 1):
+            for i in range(index - 1):
                 current = current.next
             newNode.next = current.next
             current.next = newNode    
@@ -50,7 +71,11 @@ class MyLinkedList(object):
         self.size += 1
         
     def deleteAtIndex(self, index):
-       
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        :type index: int
+        :rtype: None
+        """
         if index < 0 or index >= self.size:
             return
         
@@ -59,7 +84,7 @@ class MyLinkedList(object):
         if index == 0:
             self.head = self.head.next
         else:
-            for _ in range(index - 1):
+            for i in range(0, index - 1):
                 current = current.next
             current.next = current.next.next              
 
@@ -72,5 +97,3 @@ class MyLinkedList(object):
 # obj.addAtTail(val)
 # obj.addAtIndex(index,val)
 # obj.deleteAtIndex(index)
-
-
---
+++ DataStructures/Linkedlist/linkedlistDesign.py
@@ -0,0 +1,80 @@
+class Node:
+    def __init__(self,val):
+        self.val=val
+        self.next=None
+        
+class Linkedlist:
+    def __init__(self):
+        self.head=None
+        self.size=0
+        
+    def get(self,index):
+        if index<self.size or index<0:
+            return
+        
+        curr=self.head
+        for _ in range(index):
+            curr=curr.next
+        return curr.val
+        
+    def addAtIndex(self,index,val):
+        
+        if index<0 or index>self.size:
+            return
+        
+        to_add=Node(val)
+        curr=self.head
+        for _ in range(index-1):
+            curr=curr.next
+        to_add.next=curr.next
+        curr.next=to_add
+        self.size+=1
+    
+    def addAtHead(self,val):
+        self.addAtIndex(0,val)
+         
+    def addAtTail(self,val):
+        self.addAtIndex(self.size,val)
+        
+    def deleteAtIndex(self,index):
+        
+        if index<0 or index>=self.size:
+            return 
+        
+        curr=self.head
+        for _ in range(index-1):
+            curr=curr.next
+            
+        curr.next=curr.next.next
+        self.size-=1
+        
+      
+      
+      
+
+#create Nodes        
+first=Node(5)
+sec=Node(7)
+third=Node(9)
+tail=Node(11)
+ll=Linkedlist()
+
+
+#linking the nodes and increament size of ll
+ll.head=first
+ll.size+=1
+
+first.next=sec
+ll.size+=1
+
+sec.next=third
+ll.size+=1
+
+third.next=tail
+ll.size+=1
+
+#param=ll.get(2) #get a value
+'''
+ll.deleteAtIndex(2)
+'''
+
