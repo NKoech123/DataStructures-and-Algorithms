f09bd18d10720c72cef486a7054a827df7238d18
Nicholas Koech <nicholask320@gmail.com>
2022-01-10 23:10:35 -0800

    Update findMin_Rotate.py

--- Algorithms/Binary Search/findMin_Rotate.py
+++ Algorithms/Binary Search/findMin_Rotate.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        l = 0
+        r = len(nums) - 1
+        while l < r:
+            mid = (r - l) // 2 + l
+            if nums[l] < nums[mid]:
+                r = mid
+            elif nums[mid] > nums[r]:
+                l = mid + 1
+            else:
+                return nums[l]
+        return nums[l]
