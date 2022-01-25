8930c0dfc1369a84977fff388f8a6510f31b7db9
Nicholas Koech <nicholask320@gmail.com>
2022-01-24 23:56:54 -0800

    Create NumOfIslands.py

---
+++ Algorithms/Graphs/DFS/NumOfIslands.py
@@ -0,0 +1,44 @@
+'''
+Time complexity : O (M*N)
+
+Space complexity : worst case O(M*N) in case that the grid map is filled with lands where DFS goes by M*N deep.
+
+'''
+
+class Solution:
+    def dfs(self,row,col,grid):
+        
+        
+        if row<0 or row>=len(grid) or col<0 or col>=len(grid[0]) or grid[row][col]=="0":
+            return 
+        
+        grid[row][col]="0"
+        
+        self.dfs(row+1,col,grid)
+        self.dfs(row-1,col,grid)
+        self.dfs(row,col+1,grid)
+        self.dfs(row,col-1,grid)
+       
+        
+    def numIslands(self, grid: List[List[str]]) -> int:
+        
+        if not grid or len(grid)==0:
+            return 0
+
+        num_row, num_col, islands= len(grid), len(grid[0]), 0
+        
+        visited=set() 
+            
+        for r in range(num_row):
+            for c in range(num_col):
+                
+                if grid[r][c]=="1" and (r,c) not in visited:
+                    islands+=1
+                    self.dfs(r,c,grid)
+                    
+                    
+        return islands
+                           
+        
+        
+        
