37a2f6da4a6237b027b62c72a04cb20bacac576c
Nicholas Koech <nicholask320@gmail.com>
2022-02-02 14:03:52 -0800

    Update validParentheses.py

--- DataStructures/String/validParentheses.py
+++ DataStructures/String/validParentheses.py
@@ -1,29 +1,35 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         '''
-        tests stacks
-        
         s="{[]}"
         
         s="()[]{}"
         
-        Time O(N) - linear time thru' iterating string s
-        Space O(N) - because of stack
+        stack=['(']
+        -create empty stack stack.
+        -create a hashmap that close->opening
+        -loop thru' the s searching if s[char] in map:
+              -if it's in map, that's a closing and so there should be an opening in the stack.
+              -if the s[char] is not in the map, means it's an opening,so append it to stack.
+        
         '''
         
         stack=[]
         
-        closeToOpen=  {")": "(", "}": "{", "]": "["}
+        closeToOpen={')':'(' ,   '}':'{' ,   ']':'['  }
+        
+        
         
         for c in s:
+            
             if c in closeToOpen:
-                if stack and stack[-1] == closeToOpen[c]:
+                if stack and stack[-1]==closeToOpen[c]:
                     stack.pop()
-                    
                 else:
                     return False
+        
             else:
                 stack.append(c)
                 
+        
         return True if not stack else False
-            
