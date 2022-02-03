0a2ed5d0a0fb2c093f5121a2231a97bd4998c6a0
Nicholas Koech <nicholask320@gmail.com>
2022-02-02 16:06:15 -0800

    Create palindromicSubstrings.py

---
+++ DataStructures/String/palindromicSubstrings.py
@@ -0,0 +1,30 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        
+        '''
+        account for odd and even length. traverse the s while making "circles" at each point
+        
+        '''
+        
+        count=0
+        
+        for i in range(len(s)):
+            
+            #odd length
+            l,r=i,i
+            
+            while l>=0 and r<len(s) and s[l]==s[r]:
+                count+=1
+                l-=1
+                r+=1
+                
+            #even length
+            l,r=i,i+1
+            while l>=0 and r<len(s) and s[l]==s[r]:
+                count+=1
+                l-=1
+                r+=1
+                
+                
+        return count
+                
