e026de792158e408620b06f2448ff3da82a0282b
Nicholas Koech <nicholask320@gmail.com>
2022-02-02 18:50:29 -0800

    Create EncodeDecodeStrings.py

---
+++ DataStructures/String/EncodeDecodeStrings.py
@@ -0,0 +1,36 @@
+class Codec:
+    def encode(self, strs: [str]) -> str:
+        """Encodes a list of strings to a single string.
+        
+        "Hello, World"-----encode---> "4#Hello4#World"
+         -use len(s)+"#"+ s to encode
+        """
+        res=""
+        for s in strs:
+            
+            res+= str(len(s)) + "#" + s
+            
+        return res
+        
+
+    def decode(self, s: str) -> [str]:
+        """Decodes a single string to a list of strings.
+        
+        s = "5# Hello 5#World"
+        """
+        res, i= [], 0
+        
+        while i < len(s):
+            
+            j=i
+            
+            while s[j] != "#":
+                j+=1
+                
+            length = int(s[i:j])
+            
+            res.append(s[j+1:j+1+length])
+            
+            i=j + length + 1
+            
+        return res
