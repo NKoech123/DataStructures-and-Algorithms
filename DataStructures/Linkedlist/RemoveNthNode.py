7a1e7958774d2225f8f4281e026925a575294e99
Nicholas Koech <nicholask320@gmail.com>
2022-02-04 00:02:29 -0800

    Update RemoveNthNode.py

--- DataStructures/Linkedlist/RemoveNthNode.py
+++ DataStructures/Linkedlist/RemoveNthNode.py
@@ -1,16 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        #two-pointer
-        left=right=head
+        '''
+        -create dummyhead,then start fast and slow pointers at dummyhead
+        -run the fast pointer n steps head, then run both pointers
+        until fast pointer is at the tail.
+        -then remove the nth node(the node next to slow node)
+        -Time 0(L) L is length of the LL
+        -Space 0(1), we only used constant extra space
+     
+        '''
+        
+        dummyhead=ListNode(-1,head)
+        
+        fast=slow=dummyhead
         
         for _ in range(n):
-            right=right.next
+            fast=fast.next
             
-        if right is None:
-            return head.next
-        while right.next:
-            left=left.next
-            right=right.next
-        left.next=left.next.next
+        if fast is None: #means the nth node is not in LL
+            return head 
         
-        return head
+        while fast.next:
+            fast=fast.next
+            slow=slow.next
+        slow.next=slow.next.next
+        
+        
+        return dummyhead.next
