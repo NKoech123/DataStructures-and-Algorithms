86b0243c83906c603594a11ce51ed69642d8dac1
Nicholas Koech <nicholask320@gmail.com>
2022-02-05 00:54:42 -0800

    Update LeetCode_12_Cointainer_With_Mostwater.py

--- DataStructures/Arrays_Strings/LeetCode_12_Cointainer_With_Mostwater.py
+++ DataStructures/Arrays_Strings/LeetCode_12_Cointainer_With_Mostwater.py
@@ -1,17 +1,42 @@
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         '''
         height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
-                   
-        '''
-        max_area=0
-        l,r=0,len(height)-1
+                  i                       j
+        Shrinking window:
+        check if i<j or otherwise:
+           -if i<j : compute area, then i+=1
+           -if i>j : compute area, then j-=1
         
-        while l<r:
-            max_area=max(max_area,min(height[l],height[r])*(r-l))
+        take min([height[i],height[j]]) * (j-i)
+        
+        Time O(N) -single pass
+        Space O(1) -constant space is used
+        '''
+        i , j = 0 , len(height)-1
+        
+        max_area , local_area = 0 , 0
+        
+        while i<j:
             
-            if height[l]<height[r]:
-                l+=1
+            if height[i]<=height[j]:
+                
+                local_area = ( min([height[i],height[j]]) * (j-i) )
+                
+                max_area = max(max_area,local_area)
+                
+                i+=1
+            
             else:
-                r-=1
+                
+                local_area = ( min([height[i],height[j]]) * (j-i) )
+                
+                max_area = max(max_area,local_area)
+                
+                j-=1
+                
         return max_area
+        
+       
+            
