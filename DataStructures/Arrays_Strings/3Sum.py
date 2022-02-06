2fd66cefe3d411383a01148f2123817c561e4dbb
Nicholas Koech <nicholask320@gmail.com>
2022-02-05 20:42:32 -0800

    Update 3Sum.py

--- DataStructures/Arrays_Strings/3Sum.py
+++ DataStructures/Arrays_Strings/3Sum.py
@@ -67,4 +67,26 @@ class Solution:
                 lo+=1
                 while lo<hi and nums[lo]==nums[lo-1]:
                     lo+=1
+        
+    def threeSum_WithSet(self, nums: List[int]) -> List[List[int]]:
+        '''
+        nums = [-1, 0, 1, 2, -1, -4]
+                 i  
+        '''
+        res=set()
+        dups=set()
+        
+        seen={}
+        
+        for i,val1 in enumerate(nums):
+            if val1 not in dups:
+                dups.add(val1)
+                
+                for j,val2 in enumerate(nums[i+1:]):
+                    complement = -val1-val2
+                    if complement in seen and seen[complement] == i:
+                        res.add(tuple(sorted((val1,val2,complement))))
+                    seen[val2] = i
+                    
+        return res
                     
