2b56204b8c25e867a7f2a404a8e2919d2f5e0045
Nicholas Koech <nicholask320@gmail.com>
2022-02-24 02:08:10 -0800

    Update longestRepeatingChars.py

--- DataStructures/String/longestRepeatingChars.py
+++ DataStructures/String/longestRepeatingChars.py
@@ -7,13 +7,15 @@ class Solution:
           -in any window, in inorder to obtain the longest repeating characters, it makes sense to replace the least characters and 
           leave the most characters. If the least characters in a window is more than K, let's shrink the window from the left my decrementing the 
           leftmost character and incrementing the left pointer by 1. Then store the max size of the window
+          
+          You only want to deal with a window in which the least frequent chars is less than or equal to k, so you're saying: 
+          While the count of your least frequent chars is greater than k then it's not valid so you want to decrement the size 
+          of the window by removing an element from the left and you increment the left pointer.
         
         "A   A   B   A   B   B    A"
         LR
          count={'A':1}
         '''
-        
-        
         size=0
         count={}
         
