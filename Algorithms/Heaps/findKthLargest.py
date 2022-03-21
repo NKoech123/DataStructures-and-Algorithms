7d2ed2595c655c0bda792caa959359b4d6c33d1d
Nkoech123 <nicholask320@gmail.com>
2022-03-21 14:23:15 -0700

    heap

---
+++ Algorithms/Heaps/findKthLargest.py
@@ -0,0 +1,22 @@
+import unittest
+class Solution:
+    def findKthLargest(self,nums  , k:int ) -> int:
+        import heapq
+
+        return heapq.nlargest(k,nums)[-1]
+
+class TestingSolution(unittest.TestCase):
+    def test_funcRes(self):
+        kth=Solution().findKthLargest([3,2,3,1,2,4,5,5,6] , 4 ) 
+        self.assertEqual(kth,4)
+
+if __name__ == "__main__":
+    k,nums=4, [3,2,3,1,2,4,5,5,6]
+    
+    kth=Solution().findKthLargest(nums , k ) 
+
+    print(kth)
+    
+    unittest.main()
+
+
