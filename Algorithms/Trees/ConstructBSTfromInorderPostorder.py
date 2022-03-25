0e1a786c7ad2b945c0ee3a0c62f2f42c286e6f07
Nicholas Koech <nicholask320@gmail.com>
2022-03-25 15:56:20 -0700

    Update ConstructBSTfromInorderPostorder.py

--- Algorithms/Trees/ConstructBSTfromInorderPostorder.py
+++ Algorithms/Trees/ConstructBSTfromInorderPostorder.py
@@ -28,3 +28,34 @@ class Solution:
         root.right = self.buildTree(preorder, right_tree)
         
         return root
+    
+
+
+class More_Optimal_Solution:
+    '''
+    Use of hashmap to provive 0(1) time when indexing inorder
+    '''
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        
+        if not preorder or not inorder:
+            return None
+        
+        inorder_cache = {} 
+        for idx,val in enumerate(inorder):
+            inorder_cache[val]=idx
+        
+        def helper (preorder, inorder):
+            if not preorder or not inorder:
+                return None
+            
+            root_val = preorder.pop(0)
+            left_tree= inorder[0:inorder_cache[root_val]]
+            right_tree=inorder[inorder_cache[root_val] + 1:]
+
+        
+            root = TreeNode(root_val)
+            root.left = self.buildTree(preorder,left_tree)
+            root.right =self.buildTree(preorder,right_tree)
+
+            return root
+        return helper(preorder, inorder)
