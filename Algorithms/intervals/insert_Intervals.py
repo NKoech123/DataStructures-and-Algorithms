59972b4d04c6e0580fa6383360d1a7b848b32547
Nicholas Koech <nicholask320@gmail.com>
2022-04-12 01:02:30 -0700

    Update insert_Intervals.py

--- Algorithms/intervals/insert_Intervals.py
+++ Algorithms/intervals/insert_Intervals.py
@@ -10,12 +10,13 @@ class Solution:
                 res.append(newInterval)
                 return res + intervals[i:]
             
-            #check if newInterval comes after interval intervvals[i]
+            #check if newInterval comes after interval intervals[i]
             elif newInterval[0]  > intervals[i][1]:
                 res.append(intervals[i])
                 
             else:
-                newInterval = [min(newInterval[0],intervals[i][0]) , max(newInterval[1],intervals[i][1])]
+                newInterval = [min(newInterval[0],intervals[i][0]) ,
+                               max(newInterval[1],intervals[i][1])]
                 
         res.append(newInterval)
         
