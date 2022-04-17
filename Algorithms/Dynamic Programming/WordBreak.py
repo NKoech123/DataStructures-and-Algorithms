cbb201fcc410370cc8d8c305e23cd2ca2baf2cd4
Nicholas Koech <nicholask320@gmail.com>
2022-04-16 22:52:56 -0700

    Update WordBreak.py

--- Algorithms/Dynamic Programming/WordBreak.py
+++ Algorithms/Dynamic Programming/WordBreak.py
@@ -24,3 +24,36 @@ class Solution:
                     dp[i]=True
                     break
         return dp[len(s)]
+    
+    
+ 
+    def wordBreak_reverse(self, s: str, wordDict: List[str]) -> bool:
+        '''
+       "dog","sand","and","cat"]  FALSE
+         
+         s = "applepenapple"
+         wordDict = ["apple","pen"]  TRUE
+        
+         s = "leetcode"
+         wordDict = ["leet","code"]     TRUE
+         
+             "l    e     e     t     c     o     d      e"
+        dp=[False False False False True False False False True ]
+                                     i   
+        '''
+        dp = [False]* (len(s)+1)
+        
+        dp[len(s)] = True
+        
+        for i in reversed(range(len(s))):
+            for w in wordDict:
+                if ( (i+len(w)) <= len(dp) 
+                    and s[i:i+len(w)] == w):
+                    
+                    dp[i] = dp[i+len(w)]
+                    
+                if dp[i]:
+                    break
+                    
+        return dp[0]
+                    
