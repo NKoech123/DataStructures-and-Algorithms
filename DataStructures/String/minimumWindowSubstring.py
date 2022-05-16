9011ec728909a44641059ec6f3b2bb4bd9d25dbf
Nicholas Koech <nicholask320@gmail.com>
2022-05-16 11:22:20 -0700

    Update minimumWindowSubstring.py

--- DataStructures/String/minimumWindowSubstring.py
+++ DataStructures/String/minimumWindowSubstring.py
@@ -1,40 +1,42 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-     
-        #base case
-        if t=="" or s=="": return ""
+        ''' 
+        s = "A D O B E C  O D  E B A  N  C"
+             lr
         
-        #creating target map
-        target_map,window_map={},{}
+        '''
+        
+        if t =='' or s == '' : return ''
+        
+        target_map = {}
+        
         for char in t:
-            target_map[char]=1+target_map.get(char,0)
+            target_map[char] = 1 + target_map.get(char,0)
+                
+        need, have = len(target_map) , 0
         
-
-        have,need=0,len(target_map)
-        res,resLen=[-1,-1], float("infinity")
+        window_map = {}
+        res, resLen = [-1,-1], float("infinity")
         l=0
         for r in range(len(s)):
             window_map[s[r]] = 1 + window_map.get(s[r],0)
-         
-            
-            if s[r] in target_map and target_map[s[r]]==window_map[s[r]]:
+  
+            if s[r] in target_map and window_map[s[r]] == target_map[s[r]]:
                 have+=1
-              
-            
-            while have==need :
-              
-                #update result
-                if resLen > r-l+1 :
-                    resLen=r-l+1
-                    res=[l,r]
-                    
-                #pop from the left of the window
-                window_map[s[l]]-=1
                 
-                if s[l] in target_map and window_map[s[l]]<target_map[s[l]]:
+            while have == need:
+                
+                if r-l+1 < resLen:
+                    res =[l,r]
+                    resLen = r-l+1
+                
+                window_map[s[l]]-=1
+            
+                if s[l] in target_map and window_map[s[l]] < target_map[s[l]]:
                     have-=1
                 l+=1
                     
-        l,r=res  
-        return s[l:r+1] if resLen!=float('infinity') else ''
+        l,r = res
+        
+        return s[l:r+1] if resLen!= float('infinity') else ''
             
