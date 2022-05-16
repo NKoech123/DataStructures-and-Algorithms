f5ff51a856180b37594dabc1b04f6e3a4dac1680
Nicholas Koech <nicholask320@gmail.com>
2022-05-16 15:16:51 -0700

    Update validPalindrome.py

--- DataStructures/String/validPalindrome.py
+++ DataStructures/String/validPalindrome.py
@@ -1,23 +1,23 @@
+'''
+use left and right two-pointers and compare chars from both ends while shrinking. Make sure all
+chars are alphanumeric, and assume lower case for comaprison.
+'''
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        '''
-        use left and right two-pointers and compare chars from both ends while shrinking. Make sure all
-        chars are alphanumeric, and assume lower case for comaprison.
-        '''
         
-        i , j = 0 , len(s)-1
+        l,r = 0,len(s)-1
         
-        while i<j:
+        while l<r:
             
-            while i<j and not s[i].isalnum() :
-                i+=1
-                
-            while i<j and not s[j].isalnum():
-                j-=1
-                
-            if s[i].lower()!=s[j].lower():
+            while l<r and not s[l].isalnum():
+                l+=1
+            while l<r and not s[r].isalnum():
+                r-=1
+            
+            if s[l].upper() != s[r].upper():
                 return False
-            i+=1
-            j-=1
+            l+=1
+            r-=1
             
         return True
+            
