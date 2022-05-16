701c38cdb55e93398f68a49e6bf434c5331ef4fe
Nicholas Koech <nicholask320@gmail.com>
2022-05-16 13:25:08 -0700

    Update validAnagram.py

--- DataStructures/String/validAnagram.py
+++ DataStructures/String/validAnagram.py
@@ -25,3 +25,19 @@ class Solution:
                 return False
         return True
         
+
+    def isAnagram_sol2(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t): return False
+        
+        count = [0]*26
+        
+        for char in s:
+            count[ord(char)-ord('a')] +=1
+            
+        for char in t:
+            count[ord(char)-ord('a')] -=1
+            if count[ord(char)-ord('a')] < 0:
+                return False
+            
+        return sum(count) == 0
