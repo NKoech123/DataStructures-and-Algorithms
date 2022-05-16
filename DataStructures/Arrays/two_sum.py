69c63fba8420d4b28f21338318125f343590495d
Nicholas Koech <nicholask320@gmail.com>
2022-05-16 15:58:41 -0700

    Update two_sum.py

--- DataStructures/Arrays/two_sum.py
+++ DataStructures/Arrays/two_sum.py
@@ -1,10 +1,30 @@
-#Leetcode Problem 1
-def twoSum(nums, target) :
-
-    hashmap={}
-    for i in range(len(nums)):
-        complimentary_val=target-nums[i]
-        if complimentary_val in hashmap:
-            return [i, hashmap[complimentary_val]]
-        hashmap[nums[i]]=i
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        '''
+        nums = [2,3,2, 3,3,3,3,3,7]  target = 9
+                    i            j
+        hashmap = {}
+        
+        N*N
+        
+        
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i]+nums[j] == target:
+                    return [i,j]
+                
+        [2,7,11,15]  t=9
+       
+        
+          '''
+        hashmap = {}
+        
+        for i in range(len(nums)):
+            
+            comp = target - nums[i]
+            
+            if nums[i] in hashmap:
+                return [i, hashmap[nums[i]]]
+            
+            hashmap[comp] = i
             
