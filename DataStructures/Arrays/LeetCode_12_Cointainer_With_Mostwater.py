b98078f7331a2fdd2a39c779e53eb6c29fef0133
Nicholas Koech <nicholask320@gmail.com>
2022-05-18 10:15:09 -0700

    Update LeetCode_12_Cointainer_With_Mostwater.py

--- DataStructures/Arrays/LeetCode_12_Cointainer_With_Mostwater.py
+++ DataStructures/Arrays/LeetCode_12_Cointainer_With_Mostwater.py
@@ -38,5 +38,30 @@ class Solution:
                 
         return max_area
         
-       
+
+
+    def maxArea_anotherSol(self, height: List[int]) -> int:
+        '''
+        
+        [1, 8, 6, 2, 5, 4, 8, 3, 7]
+                r
+            l
+        '''
+        
+        area, max_area = 0,0
+        
+        l, r = 0, len(height)-1
+        
+        while l<r:
+            
+            area = min(height[l], height[r]) * (r-l)
+            max_area = max (max_area, area)
+            
+            if height[l] <= height[r]:
+                l+=1
+                
+            else:
+                r-=1
+                
+        return max_area
             
