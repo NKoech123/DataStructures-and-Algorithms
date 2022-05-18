f66be41deab73b135656bc3c381f5b3219f5ae0b
Nicholas Koech <nicholask320@gmail.com>
2022-05-18 15:05:37 -0700

    Update longestCommonSubsequence.py

--- Algorithms/Dynamic Programming/longestCommonSubsequence.py
+++ Algorithms/Dynamic Programming/longestCommonSubsequence.py
@@ -1,12 +1,6 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        '''
-        
-        
-        
-        
-        '''
-        
+   
         dp= [ [0]*(len(text2)+1) for _ in range(len(text1)+1)]
         
         
