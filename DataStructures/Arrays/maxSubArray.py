da62c2ae7463de37450c75bdf9ef825ac43d4709
Nicholas Koech <nicholask320@gmail.com>
2022-05-18 10:11:29 -0700

    Update maxSubArray.py

--- DataStructures/Arrays/maxSubArray.py
+++ DataStructures/Arrays/maxSubArray.py
@@ -4,7 +4,6 @@ class Solution:
         '''
         Time O(N)
         Space O(N)- storing the array
-        
         '''
         
         n=[0 for i in range(len(nums))]
@@ -12,14 +11,13 @@ class Solution:
         n[0]=nums[0]
         for i in range(1,len(nums)):
             n[i]=max(n[i-1]+nums[i],nums[i])
-            
         return max(n)
       
 
     def maxSubArray_SlidingWindow(self, nums: List[int]) -> int:
         '''
-       Time 0(N)
-       Space 0(1)
+         Time 0(N)
+         Space 0(1)
         '''
         maxSub=nums[0]
         currSum=0
@@ -37,15 +35,12 @@ class Solution:
 
     def maxSubArray_another(self, nums: List[int]) -> int:
         '''  
-        
         [5, 4, -1, 7, 8]
             i
            curr_sub= max(nums[i], nums[i]+curr_sub)
-       curr_sub = nums[0]
+           curr_sub = nums[0]
        
        iterate 1:len(nums)
- 
-    
         '''
         
         curr_sub = nums[0]
