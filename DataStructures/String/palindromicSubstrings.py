4f1c183b39f0345d6aedaf04795b322d59b62434
Nicholas Koech <nicholask320@gmail.com>
2022-05-17 22:06:05 -0700

    Update palindromicSubstrings.py

--- DataStructures/String/palindromicSubstrings.py
+++ DataStructures/String/palindromicSubstrings.py
@@ -30,4 +30,29 @@ class Solution:
                 
                 
         return count
-                
+  
+    def countSubstrings_another(self, s: str) -> int:
+        
+        count = 0
+        
+        for i in range(len(s)):
+            
+            #odd
+            count += self.countPalindrome(s, i, i)
+            
+            #even
+            count += self.countPalindrome(s, i, i+1)
+            
+        return count
+    
+    
+    def countPalindrome(self, s, lo, hi):
+        
+        count = 0
+        
+        while lo>=0 and hi<len(s) and s[lo] == s[hi]:
+            count +=1
+            lo-=1
+            hi+=1
+            
+        return count
